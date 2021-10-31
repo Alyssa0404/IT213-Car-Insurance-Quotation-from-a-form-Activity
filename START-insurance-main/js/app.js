27488708ceef62fa7cf5c1447e63a3025964546e
Alyssa0404 <balaoroalyssa@gmail.com>
2021-10-31 19:34:17 +0800

    Init

--- START-insurance-main/js/app.js
+++ START-insurance-main/js/app.js
@@ -1,54 +1,82 @@
-// Variables
+//Variables
 const form = document.getElementById('request-quote');
 
-
+const html = new HTMLUI();
 
 
 //Event Listeners
-
 eventListeners();
-function eventListeners(){
-   document.addEventListener('DOMContentLoaded', function() {
-        //Create the <option> for the years
-       const html = new HTMLUI();
-       HTMLUI.prototype.diplayYears();
-    });
-    
-    
-   form.addEventListener('submit', function(e){
-   e.preventDefault();
 
-      const make = document.getElementById('make').value;
-      const year = document.getElementById('year').value;
-
-     const level = document.querySelector('input[name="level"]:checked').value;
+function eventListeners() {
+    document.addEventListener('DOMContentLoaded', function() {
+        //Create the options for the Years
+       
+        html.displayYears();
     
-        if(make === '' || year === '' || level === '') {
-            console.log('Error');
-        } else {
-            console.log('Alright!!');
-        }
+    }); 
+    
+    //When the form is submitted
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+
+         //Read the values from the form
+    const make = document.getElementById('make').value;
+    const year = document.getElementById('year').value;
+    //Read the radio buttons
+    const level = document.querySelector('input[name="level"]:checked').value;
+
+    //Check that all the fields have something
+    if( make === '' || year === '' || level === '' ) {
+       html.displayError('All the fields are mandatory');
+
+    } else {
+        console.log('Allright!');
+    }
+
     });
+ 
 }
-
-
-
 
 
 //Objects
 
 function HTMLUI() {}
 
-HTMLUI.prototype.diplayYears = function () {
-
+//Displays the latest years in the select
+HTMLUI.prototype.displayYears = function() {
+    //Max and Minimum Years
     const max = new Date().getFullYear(),
-        min = max - 20;
+          min = max - 20;
 
+    //Generate the list with the 20 years
     const selectYears = document.getElementById('year');
-    for (let i= max; i >= min; i-- ) {
+
+    //Print the values
+    for(let i = max; i > min; i--) {
         const option = document.createElement('option');
         option.value = i;
         option.textContent = i;
         selectYears.appendChild(option);
+        
     }
+   
+}
+
+//Prints an error
+HTMLUI.prototype.displayError = function (message) {
+    //create a div
+    const div = document.createElement('div');
+    div.classList = 'error';
+
+    //Insert the Message
+    div.innerHTML = `
+        <p>${message}</p>
+    `;
+
+    form.insertBefore(div, document.querySelector('.form-group'));
+
+    //Remove the error
+    setTimeout(function() {
+        document.querySelector('.error').remove();
+    }, 3000);
 }
